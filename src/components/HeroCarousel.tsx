48cb4c6f1079de734e7ced04fbe0d1fb97835928
Sir_46 <fadlan.8291@gmail.com>
2023-05-21 10:42:34 +0700

    Update image

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -11,21 +11,21 @@ type Props = {
 const data = [
   {
     image:
-      'https://images.unsplash.com/photo-1504203328729-b937e8e102f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80',
-    title: 'Best forests to visit in North America',
+      'https://images.unsplash.com/photo-1552465011-b4e21bf6e79a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&w=2878&q=80',
+    title: 'Phra Nang Beach, Krabi, Thailand',
     category: 'nature',
   },
   {
     image:
-      'https://images.unsplash.com/photo-1583491470869-ca0b9fa90216?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80',
-    title: 'Hawaii beaches review: better than you think',
+      'https://images.unsplash.com/photo-1598255055837-723370293a03?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&w=2825&q=80',
+    title: 'Krabi, Thailand',
     category: 'beach',
   },
 ];
 
 const Hero = ({ className }: Props) => {
   const slides = data.map((item) => (
-    <Carousel.Slide key={item.title} className='h-96'>
+    <Carousel.Slide key={item.title} className='h-[420px]'>
       <Image
         src={item.image}
         alt={item.title}
