b0ffd2392e7e8152123bfd65bc12009e9079776c
Sir_46 <fadlan.8291@gmail.com>
2023-07-27 14:31:43 +0700

    Rename

--- src/components/PackageList.tsx
+++ src/components/PackageList.tsx
@@ -3,11 +3,11 @@ import PackageCard from './PackageItem';
 import { IPackage } from '@/types';
 
 type Props = {
-  pkgs?: IPackage[];
+  data: IPackage[];
 };
 
-const PackageList = ({ pkgs }: Props) => {
-  if (pkgs?.length === 0) {
+const PackageList = ({ data }: Props) => {
+  if (data?.length === 0) {
     return <Empty className='px-4 mt-10 md:mt-4' />;
   }
 
@@ -19,7 +19,7 @@ const PackageList = ({ pkgs }: Props) => {
         gridTemplateColumns: 'repeat(auto-fill,minmax(300px, 1fr))',
       }}
     >
-      {(pkgs || []).map((pkg: IPackage, idx: number) => (
+      {data.map((pkg: IPackage, idx: number) => (
         <PackageCard pkg={pkg} key={idx} />
       ))}
     </div>
