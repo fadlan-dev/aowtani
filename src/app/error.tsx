1ede2f7fd2930f7e900d1da248e08f9af211fc4d
Sir_46 <fadlan.8291@gmail.com>
2023-08-08 14:10:45 +0700

    Update error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,5 @@
 'use client';
-
 import { Button } from '@mantine/core';
-
 export default function Error({
   error,
   reset,
@@ -11,12 +9,14 @@ export default function Error({
 }) {
   return (
     <div
-      className='flex flex-col  justify-center mt-20 mb-20'
-      style={{ minHeight: 'calc(100vh - 80px)' }}
+      className='flex flex-col justify-center'
+      style={{ minHeight: 'calc(100vh - 142px)' }}
     >
       <div className='container block text-center'>
         <h2>{error.message || ' Something went wrong!'}</h2>
-        <p>{error.stack}</p>
+        <div>
+          <code className='whitespace-pre-wrap break-words'>{error.stack}</code>
+        </div>
         <Button mt='md' variant='outline' onClick={() => reset()}>
           Try again
         </Button>
