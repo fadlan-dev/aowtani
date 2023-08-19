64eddd0a8a6721c3656daf3b3f5c59b863c59051
Sir_46 <fadlan.8291@gmail.com>
2023-08-19 21:34:05 +0700

    Fixed "localGuide.profile" is possibly undefined

--- src/app/local-guides/[id]/page.tsx
+++ src/app/local-guides/[id]/page.tsx
@@ -1,7 +1,8 @@
+import CommunityItem from '@/components/CommunityItem';
 import LocalGuideItem from '@/components/LocalGuideItem';
 import PackageList from '@/components/PackageList';
 import { getPackages } from '@/libs/services/getPackages';
-import { ILocalGuide, IPackage } from '@/types';
+import { ILocalGuide } from '@/types';
 
 type Props = {
   params: { id: string };
@@ -14,7 +15,7 @@ export async function generateMetadata({ params }: Props) {
     description: localGuide.detail,
     openGraph: {
       images: [
-        `${process.env.NEXT_PUBLIC_URL}${localGuide.profile.asset}`,
+        `${process.env.NEXT_PUBLIC_URL}${localGuide.profile?.asset}`,
         'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80',
       ],
     },
@@ -38,7 +39,7 @@ const Page = async ({ params }: Props) => {
         <h1>โปรไฟล์ไกด์ท้องทิ่น</h1>
         <LocalGuideItem data={localGuide} />
       </div>
-      <div className='flex flex-col lg:flex-row gap-4 mt-4 p-4'>
+      <div className='flex flex-col-reverse lg:flex-row gap-4 mt-4 p-4'>
         <div className='flex-1'>
           <h2>แพ็กเกจทัวร์</h2>
           <PackageList data={pkgs} />
@@ -46,6 +47,10 @@ const Page = async ({ params }: Props) => {
 
         <div className='w-full lg:w-80'>
           <h2>สังกัดชุมชน</h2>
+          <CommunityItem
+            className='mt-2 max-w-[320px]'
+            community={localGuide.community}
+          />
         </div>
       </div>
     </div>
