60cf31abcbd546e99c136a9b71289fbbe165de6f
Sir_46 <fadlan.8291@gmail.com>
2023-08-26 22:20:31 +0700

    Add pagination

--- src/components/OrganizationList.tsx
+++ src/components/OrganizationList.tsx
@@ -6,15 +6,19 @@ import {
   TextInput,
   ActionIcon,
   useMantineTheme,
-  Flex,
+  Pagination,
 } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
 import { FunctionComponent } from 'react';
 import Empty from './Empty';
 import OrganizationItem from './OrganizationItem';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { cn } from '@/libs/utils';
 
 interface OrganizationListProps {
   data: IOrganization[];
+  total: number;
   showSearch?: boolean;
   showPagination?: boolean;
   showMore?: boolean;
@@ -25,16 +29,27 @@ interface OrganizationListProps {
 
 const OrganizationList: FunctionComponent<OrganizationListProps> = ({
   data,
+  total,
   className,
   showSearch,
-  showPagination,
-  showMore,
   title,
   subTitle,
 }) => {
   const theme = useMantineTheme();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleRoute = ({ page = '1' }: { page?: string }) => {
+    const newParams = new URLSearchParams(searchParams.toString());
+    if (page) {
+      newParams.set('page', `${page}`);
+    }
+
+    router.push(`${pathname}?${newParams}`);
+  };
   return (
-    <div>
+    <div className={cn(className)}>
       <div className='text-center px-4'>
         <Title weight='bold'>{title}</Title>
         {subTitle && <Text className='mt-2'>{subTitle}</Text>}
@@ -62,11 +77,24 @@ const OrganizationList: FunctionComponent<OrganizationListProps> = ({
       {data.length === 0 ? (
         <Empty className='px-4 mt-10 md:mt-4' />
       ) : (
-        <div className={'grid grid-cols-list gap-4 px-4 mt-4'}>
-          {data.map((organize: IOrganization) => (
-            <OrganizationItem key={organize.id} data={organize} />
-          ))}
-        </div>
+        <>
+          <div className={'grid grid-cols-list gap-4 px-4 mt-4'}>
+            {data.map((organize: IOrganization) => (
+              <OrganizationItem key={organize.id} data={organize} />
+            ))}
+          </div>
+          <div>
+            {total > 6 && (
+              <Pagination
+                total={total / 6}
+                value={Number(searchParams.get('page')) || 1}
+                size='sm'
+                className='w-fit m-auto'
+                onChange={(page) => handleRoute({ page: `${page}` })}
+              />
+            )}
+          </div>
+        </>
       )}
     </div>
   );
--- src/libs/services/getOrganizations.ts
+++ src/libs/services/getOrganizations.ts
@@ -1,10 +1,29 @@
 import { IOrganization } from '@/types';
+interface props {
+  page?: number;
+  per_page?: number;
+}
 
-export const getOrganizations = async (): Promise<IOrganization[]> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/organizations.json`,
-    { cache: 'no-store' }
-  );
+interface IResponse {
+  data: IOrganization[];
+  total: number;
+}
+
+export const getOrganizations = async ({
+  page = 1,
+  per_page = 6,
+}: props): Promise<IResponse> => {
+  let queryParams = '';
+  if (page) {
+    queryParams += `page=${page}`;
+  }
+
+  if (per_page) {
+    queryParams += `per_page=${per_page}`;
+  }
+
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/organizations.json?${queryParams}`;
+  const res = await fetch(url, { cache: 'no-store' });
 
   if (!res.ok) {
     const { error } = await res.json();
