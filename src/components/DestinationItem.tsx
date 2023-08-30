3fe1c1a64c656848ecfea9a845593378f4055b70
Sir_46 <fadlan.8291@gmail.com>
2023-08-30 15:25:09 +0700

    Remoce ดูรายละเอียด btn

--- src/components/DestinationItem.tsx
+++ src/components/DestinationItem.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { IDestination } from '@/types';
-import { AspectRatio, Button, Card, Text } from '@mantine/core';
-import { useRouter } from 'next/navigation';
+import { AspectRatio, Card, Text } from '@mantine/core';
 import { FunctionComponent } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface DestinationItemProps {
   destination: IDestination;
@@ -12,40 +12,32 @@ interface DestinationItemProps {
 const DestinationItem: FunctionComponent<DestinationItemProps> = ({
   destination,
 }) => {
-  const router = useRouter();
   return (
-    <Card padding='md' className='cursor-pointer hover:shadow transition'>
-      <Card.Section>
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            className='bg-zinc-200 object-contain'
-            src={
-              destination.banners[0]?.asset
-                ? `${process.env.NEXT_PUBLIC_URL}${destination.banners[0]?.asset}`
-                : './image.svg'
-            }
-            alt={destination.name}
-            fill
-          />
-        </AspectRatio>
-      </Card.Section>
-      <Text size='lg' weight={500} mt={8} lineClamp={1}>
-        {destination.name}
-      </Text>
-      <Text size='xs' className='text-primary' lineClamp={1}>
-        {destination.address}
-      </Text>
-      <Text lineClamp={2}>{destination.description}</Text>
-      <Button
-        variant='light'
-        fullWidth
-        mt='md'
-        radius='md'
-        onClick={() => router.push(`destination/${destination.id}`)}
-      >
-        ดูรายละเอียด
-      </Button>
-    </Card>
+    <Link href={`destination/${destination.id}`}>
+      <Card padding='md' className='cursor-pointer hover:shadow transition'>
+        <Card.Section>
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              className='bg-zinc-200 object-contain'
+              src={
+                destination.banners[0]?.asset
+                  ? `${process.env.NEXT_PUBLIC_URL}${destination.banners[0]?.asset}`
+                  : './image.svg'
+              }
+              alt={destination.name}
+              fill
+            />
+          </AspectRatio>
+        </Card.Section>
+        <Text size='lg' weight={500} mt={8} lineClamp={1}>
+          {destination.name}
+        </Text>
+        <Text size='xs' className='text-primary' lineClamp={1}>
+          {destination.address}
+        </Text>
+        <Text lineClamp={2}>{destination.description}</Text>
+      </Card>
+    </Link>
   );
 };
 
