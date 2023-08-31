da3610fd654591890512c689680065a26e4125c3
Sir_46 <fadlan.8291@gmail.com>
2023-08-31 10:32:49 +0700

    Add map, reosrt and food

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,15 @@ export const metadata = {
 
 export default async function Home() {
   const destinations = await getDestinations({
+    per_page: 3,
     search: '',
   });
   const pkgs = await getPackages({
+    per_page: 3,
     search: '',
   });
   const products = await getProducts({
+    per_page: 3,
     search: '',
   });
   return (
--- src/components/AppShellItem.tsx
+++ src/components/AppShellItem.tsx
@@ -11,11 +11,12 @@ import DestinationList from './DestinationList';
 import { FoodIcon, ResortIcon, TravelIcon } from './Icons';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { cn } from '@/libs/utils';
-import Image from 'next/image';
 import { IDestination, IPackage, IProduct } from '@/types';
 import ProductList from './ProductList';
 import PackageList from './PackageList';
 import ExploreButton from './ExploreButton';
+import DestinationMap from './DestinationMap';
+import { useCallback } from 'react';
 
 type Props = {
   destinations: { data: IDestination[]; total: number };
@@ -34,7 +35,7 @@ const Index = ({ destinations, pkgs, products }: Props) => {
       title: 'สถานที่ท่องเที่ยว',
       icon: <TravelIcon />,
       color: 'blue',
-      img: '/destination.png',
+      img: <DestinationMap />,
       active: theme.colors.blue[0],
     },
     {
@@ -52,9 +53,68 @@ const Index = ({ destinations, pkgs, products }: Props) => {
       active: theme.colors.violet[0],
     },
   ];
+
+  const contentRender = useCallback((key: string) => {
+    switch (key) {
+      case 'travel':
+        return (
+          <>
+            <center>
+              <h1>สถานที่ท่องเที่ยว</h1>
+              <p>เลือกรายการสถานที่ท่องเที่ยวตามไลฟ์สไตล์ของคุณ</p>
+            </center>
+            <DestinationList
+              total={1}
+              data={destinations.data}
+              title={MENUS.find((menu) => menu.key === t)?.title || ''}
+              className='mt-6 mb-6'
+              showMore
+            />
+            <div className='mt-10'>
+              <center>
+                <h1>แพ็กเกจทัวร์แนะนำ</h1>
+                <p>ตอบโจทย์ทุกไลฟ์สไตล์</p>
+              </center>
+              <PackageList className='mt-4 px-4' data={pkgs.data} />
+            </div>
+            <div className='mt-10'>
+              <center>
+                <h1>สินค้ายอดนิยม</h1>
+                <p>ช้อปปิ้งได้ทุกเวลา</p>
+              </center>
+              <ProductList total={1} data={products.data} />
+              {products.data.length !== 0 && (
+                <ExploreButton className='mt-2' to='product' />
+              )}
+            </div>
+          </>
+        );
+
+      case 'resort':
+        return (
+          <>
+            <center>
+              <h1>ที่พัก</h1>
+              <p>ที่ตอบโจทย์ทุกไลฟ์สไตล์</p>
+            </center>
+          </>
+        );
+
+      case 'food':
+        return (
+          <>
+            <center>
+              <h1>อาหารจานโปรด</h1>
+              <p>อร่อยทุกเมนู</p>
+            </center>
+          </>
+        );
+    }
+  }, []);
+
   return (
-    <div className='flex mt-[60px]'>
-      <div className='w-72 min-w-[288px] hidden md:block'>
+    <div className='flex pt-[60px]'>
+      <div className='w-72 min-w-[288px] h-[calc(100vh-143px)] hidden md:block'>
         <Navbar hiddenBreakpoint='sm' className='h-full'>
           {MENUS.map((menu) => (
             <Navbar.Section
@@ -71,16 +131,7 @@ const Index = ({ destinations, pkgs, products }: Props) => {
               </Group>
               {menu.img && (
                 <Box h={138} mt='xs' className='relative'>
-                  <Image
-                    src={menu.img}
-                    alt={menu.title}
-                    fill
-                    className='object-contain'
-                    sizes='(max-width: 640px) 100vw,
-                    (max-width: 1280px) 50vw,
-                    (max-width: 1536px) 33vw,
-                    25vw'
-                  />
+                  {menu.img}
                 </Box>
               )}
             </Navbar.Section>
@@ -88,34 +139,7 @@ const Index = ({ destinations, pkgs, products }: Props) => {
         </Navbar>
       </div>
       <div className='flex-auto overflow-auto py-10'>
-        <center>
-          <h1>สถานที่ท่องเที่ยว</h1>
-          <p>เลือกรายการสถานที่ท่องเที่ยวตามไลฟ์สไตล์ของคุณ</p>
-        </center>
-        <DestinationList
-          total={1}
-          data={destinations.data}
-          title={MENUS.find((menu) => menu.key === t)?.title || ''}
-          className='mt-6 mb-6'
-          showMore
-        />
-        <div className='mt-10'>
-          <center>
-            <h1>แพ็กเกจทัวร์แนะนำ</h1>
-            <p>ตอบโจทย์ทุกไลฟ์สไตล์</p>
-          </center>
-          <PackageList className='mt-4 px-4' data={pkgs.data} />
-        </div>
-        <div className='mt-10'>
-          <center>
-            <h1>สินค้ายอดนิยม</h1>
-            <p>ช้อปปิ้งได้ทุกเวลา</p>
-          </center>
-          <ProductList total={1} data={products.data} />
-          {products.data.length !== 0 && (
-            <ExploreButton className='mt-2' to='product' />
-          )}
-        </div>
+        {contentRender(searchParams.get('t') || 'travel')}
       </div>
     </div>
   );
