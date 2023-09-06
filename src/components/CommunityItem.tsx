fc75cb0b5c9079fe87644e8ba6254eb3b63bf0d1
Sir_46 <fadlan.8291@gmail.com>
2023-09-06 14:02:46 +0700

    Update CommunityItem

--- src/components/CommunityItem.tsx
+++ src/components/CommunityItem.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { AspectRatio, Button, Card, Text } from '@mantine/core';
+import { AspectRatio, Card, Text } from '@mantine/core';
 import { FunctionComponent } from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import { ICommunity } from '@/types';
 import { cn } from '@/libs/utils';
+import Link from 'next/link';
 
 interface CommunityItemProps {
   community: ICommunity;
@@ -15,47 +15,39 @@ const CommunityItem: FunctionComponent<CommunityItemProps> = ({
   className,
   community,
 }) => {
-  const router = useRouter();
   return (
-    <Card
-      padding='md'
-      className={cn(className, 'cursor-pointer hover:shadow transition')}
-    >
-      <Card.Section>
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            className='bg-zinc-200 object-contain'
-            src={
-              community.images[0]?.asset
-                ? `${process.env.NEXT_PUBLIC_URL}${community.images[0].asset}`
-                : './image.svg'
-            }
-            sizes='(max-width: 640px) 100vw,
+    <Link href={`community/${community.id}`}>
+      <Card
+        padding='md'
+        className={cn(className, 'cursor-pointer hover:shadow transition')}
+      >
+        <Card.Section>
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              className='bg-zinc-200 object-contain'
+              src={
+                community.images[0]?.asset
+                  ? `${process.env.NEXT_PUBLIC_URL}${community.images[0].asset}`
+                  : './image.svg'
+              }
+              sizes='(max-width: 640px) 100vw,
             (max-width: 1280px) 50vw,
             (max-width: 1536px) 33vw,
             25vw'
-            alt={community.name}
-            fill
-          />
-        </AspectRatio>
-      </Card.Section>
-      <Text size='lg' weight={500} mt={8}>
-        {community.name}
-      </Text>
-      <Text size='xs' className='text-primary'>
-        {community.address}
-      </Text>
-      <Text lineClamp={2}>{community.detail}</Text>
-      <Button
-        variant='light'
-        fullWidth
-        mt='md'
-        radius='md'
-        onClick={() => router.push(`community/${community.id}`)}
-      >
-        ดูรายละเอียด
-      </Button>
-    </Card>
+              alt={community.name}
+              fill
+            />
+          </AspectRatio>
+        </Card.Section>
+        <Text size='lg' weight={500} mt={8}>
+          {community.name}
+        </Text>
+        <Text size='xs' className='text-primary' lineClamp={1}>
+          {community.address}
+        </Text>
+        <Text lineClamp={2}>{community.detail}</Text>
+      </Card>
+    </Link>
   );
 };
 
