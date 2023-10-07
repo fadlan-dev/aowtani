d3ff6f51bdf902eb0d85663936c6073ac47d75e4
Sir_46 <fadlan.8291@gmail.com>
2023-10-07 17:14:33 +0700

    Add env

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: 'api.aowtani.com' }],
   },
+  env: {
+    NEXT_API_HOST: 'https://api.aowtani.com/kronos',
+    NEXT_IMAGE_HOST: 'https://api.aowtani.com',
+    NEXTAUTH_SECRET: 'my_ultra_secure_nextauth_secret',
+    NEXTAUTH_URL: 'https://aowtani.com',
+  },
 };
 
 module.exports = nextConfig;
