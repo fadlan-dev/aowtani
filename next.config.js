1d541ad57516627528524702c1b2a3346930e9e4
Sir_46 <fadlan.8291@gmail.com>
2023-10-08 14:32:00 +0700

    Add NEXT_PUBLIC_URL

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [{ hostname: 'api.aowtani.com' }],
   },
   env: {
+    NEXT_PUBLIC_URL: 'https://api.aowtani.com',
     NEXT_API_HOST: 'https://api.aowtani.com/kronos',
     NEXT_IMAGE_HOST: 'https://api.aowtani.com',
     NEXTAUTH_SECRET: 'my_ultra_secure_nextauth_secret',
