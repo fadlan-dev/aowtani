3553e778e67a327ec1617bc934fd25caa1cce098
Sir_46 <fadlan.8291@gmail.com>
2023-10-27 14:18:39 +0700

    Update next-auth

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -45,9 +45,10 @@ export const authOptions: NextAuthOptions = {
           return user;
         }
 
-        // If you return null then an error will be displayed advising the user to check their details.
-        return null;
-        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        // Return an object that will pass error information through to the client-side.
+        throw new Error(
+          JSON.stringify({ errors: user.message, status: false })
+        );
       },
     }),
   ],
