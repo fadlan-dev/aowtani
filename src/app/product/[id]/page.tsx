c2fcff26b74057eba98c6adaa4c096fcf7ffe29a
Sir_46 <fadlan.8291@gmail.com>
2023-10-28 14:45:32 +0700

    Update generateMetadata

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -12,10 +12,7 @@ export async function generateMetadata({ params }: pageProps) {
     title: product.name,
     description: product.details,
     openGraph: {
-      images: [
-        `${process.env.NEXT_IMAGE_HOST}${product.images?.[0].asset}`,
-        'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80',
-      ],
+      images: [`${process.env.NEXT_IMAGE_HOST}${product.images?.[0].asset}`],
     },
   };
 }
