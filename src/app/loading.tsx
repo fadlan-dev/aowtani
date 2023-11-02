31302d5e36dabe12c8ebb92c08ed7c6a5640f9b7
Sir_46 <fadlan.8291@gmail.com>
2023-11-02 13:16:42 +0700

    Update loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image';
-
 type Props = {};
 
 const Loading = (props: Props) => {
   return (
     <div className='h-screen flex items-center justify-center'>
-      <Image width={112} height={80} src='./logo.svg' alt='loading' />
+      <span className='loader' />
     </div>
   );
 };
