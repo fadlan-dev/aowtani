edc6a324d96fb06b5fa7eb4486848c0a73474a73
Sir_46 <fadlan.8291@gmail.com>
2023-11-27 00:12:24 +0700

    Update metadata

--- src/app/calendar/[id]/page.tsx
+++ src/app/calendar/[id]/page.tsx
@@ -16,10 +16,7 @@ export async function generateMetadata({ params }: Props) {
     title: event.name,
     description: event.address,
     openGraph: {
-      images: [
-        // `${process.env.NEXT_PUBLIC_URL}${event.banners[0]}`,
-        'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80',
-      ],
+      images: [`${process.env.NEXT_PUBLIC_URL}${event.banners[0]}`],
     },
   };
 }
