a5cd222ccfef2d4831cdc94059874be8e92f7fdb
Sir_46 <fadlan.8291@gmail.com>
2023-12-06 14:55:57 +0700

    แก้ share to facebook

--- src/components/EventShare.tsx
+++ src/components/EventShare.tsx
@@ -1,20 +1,40 @@
 'use client';
-import { Avatar } from '@mantine/core';
-import { IconBrandFacebook, IconWorldWww } from '@tabler/icons-react';
+import { ActionIcon, CopyButton, Tooltip } from '@mantine/core';
+import { IconBrandFacebook, IconCheck, IconCopy } from '@tabler/icons-react';
 import { FunctionComponent } from 'react';
+import { FacebookShareButton } from 'next-share';
 
 interface EventShareProps {}
 
 const EventShare: FunctionComponent<EventShareProps> = () => {
   return (
     <div className='flex items-center p-2 gap-2'>
-      <Avatar radius='xl' color='primary' size='sm'>
-        <IconBrandFacebook size={18} />
-      </Avatar>
-
-      <Avatar radius='xl' color='primary' size='sm'>
-        <IconWorldWww size={18} />
-      </Avatar>
+      <FacebookShareButton
+        url={`${window.location.href}`}
+        quote={'next-share is a social share buttons for your next React apps.'}
+      >
+        <ActionIcon variant='light' radius='lg' color='primary'>
+          <IconBrandFacebook size={16} />
+        </ActionIcon>
+      </FacebookShareButton>
+      <CopyButton value={`${window.location.href}`} timeout={2000}>
+        {({ copied, copy }) => (
+          <Tooltip
+            label={copied ? 'Copied' : 'Copy'}
+            withArrow
+            position='right'
+          >
+            <ActionIcon
+              variant='light'
+              radius='lg'
+              color={copied ? 'teal' : 'primary'}
+              onClick={copy}
+            >
+              {copied ? <IconCheck size='1rem' /> : <IconCopy size='1rem' />}
+            </ActionIcon>
+          </Tooltip>
+        )}
+      </CopyButton>
     </div>
   );
 };
