756a195b3f3c1151746f1aa34eeb08860cb057e6
Sir_46 <fadlan.8291@gmail.com>
2023-12-10 14:50:32 +0700

    แก้urlปุ่มshareและcopy

--- src/components/EventShare.tsx
+++ src/components/EventShare.tsx
@@ -3,21 +3,25 @@ import { ActionIcon, CopyButton, Tooltip } from '@mantine/core';
 import { IconBrandFacebook, IconCheck, IconCopy } from '@tabler/icons-react';
 import { FunctionComponent } from 'react';
 import { FacebookShareButton } from 'next-share';
+import { usePathname } from 'next/navigation';
 
 interface EventShareProps {}
 
 const EventShare: FunctionComponent<EventShareProps> = () => {
+  const pathname = usePathname();
+
+  const url = `${process.env.NEXT_PUBLIC_URL}${pathname}`;
   return (
     <div className='flex items-center p-2 gap-2'>
       <FacebookShareButton
-        url={`${window.location.href}`}
+        url={url}
         quote={'next-share is a social share buttons for your next React apps.'}
       >
         <ActionIcon variant='light' radius='lg' color='primary'>
           <IconBrandFacebook size={16} />
         </ActionIcon>
       </FacebookShareButton>
-      <CopyButton value={`${window.location.href}`} timeout={2000}>
+      <CopyButton value={url} timeout={2000}>
         {({ copied, copy }) => (
           <Tooltip
             label={copied ? 'Copied' : 'Copy'}
