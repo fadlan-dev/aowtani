5ae7652f652f4d790fed8a5f53255c7ff613cd0c
Fitri <fitdeveloper97@gmail.com>
2024-02-04 19:11:11 +0700

    update menu

--- src/app/product/[id]/checkout/page.tsx
+++ src/app/product/[id]/checkout/page.tsx
@@ -1,14 +1,14 @@
-'use client';
-import PaymentMethod from '@/components/PaymentMethod';
-import OrderForm from '@/components/forms/OrderForm';
-import { useGetProduct } from '@/hooks/useGetProduct';
-import { numberFormat } from '@/libs/utils';
-import { IProduct } from '@/types';
-import { Card, Divider, Flex, Loader, Title } from '@mantine/core';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
-import { useCallback } from 'react';
+"use client";
+import PaymentMethod from "@/components/PaymentMethod";
+import OrderForm from "@/components/forms/OrderForm";
+import { useGetProduct } from "@/hooks/useGetProduct";
+import { numberFormat } from "@/libs/utils";
+import { IProduct } from "@/types";
+import { Card, Divider, Flex, Loader, Title } from "@mantine/core";
+import Image from "next/image";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 
 interface pageProps {
   params: { id: string };
@@ -16,7 +16,7 @@ interface pageProps {
 
 const Page = ({ params }: pageProps) => {
   const searchParams = useSearchParams();
-  const quantity = searchParams.get('quantity');
+  const quantity = searchParams.get("quantity");
 
   const {
     data: product,
@@ -32,86 +32,86 @@ const Page = ({ params }: pageProps) => {
   );
 
   return (
-    <div className='mt-20 mb-20'>
+    <div className="mt-20 mb-20">
       <center>
         <h1>ยืนยันการชำระเงิน</h1>
         <p>โปรดตรวจสอบข้อมูลให้ถูกต้องก่อนกดชำระเงิน</p>
       </center>
-      <div className='container'>
-        <div className='flex flex-col lg:flex-row gap-4 mt-4'>
-          <div className='flex-1 '>
+      <div className="container">
+        <div className="flex flex-col lg:flex-row gap-4 mt-4">
+          <div className="flex-1 ">
             {isFetching && !isFetched ? (
               <center>
-                <Loader variant='dots' />
+                <Loader variant="dots" />
               </center>
             ) : (
               <Card>
                 <Title order={3}>ข้อมูลการสั่งซื้อ</Title>
-                <div className='mt-6'>
-                  <table className='table-auto w-full'>
+                <div className="mt-6">
+                  <table className="table-auto w-full">
                     <thead>
                       <tr>
-                        <th className='text-start'>ยอดชำระทั้งหมด</th>
-                        <th className='text-end whitespace-nowrap'>
+                        <th className="text-start">ยอดชำระทั้งหมด</th>
+                        <th className="text-end whitespace-nowrap">
                           ราคาต่อท่าน
                         </th>
-                        <th className='text-end'>จำนวน</th>
-                        <th className='text-end whitespace-nowrap'>
+                        <th className="text-end">จำนวน</th>
+                        <th className="text-end whitespace-nowrap">
                           รวม (บาท)
                         </th>
                       </tr>
                     </thead>
-                    <tbody className='mt-4'>
+                    <tbody className="mt-4">
                       <tr>
                         <td>
-                          <Flex align='center' gap='sm' wrap='wrap'>
+                          <Flex align="center" gap="sm" wrap="wrap">
                             <Image
                               src={`${process.env.NEXT_IMAGE_HOST}${product?.images[0].asset}`}
-                              className='object-cover'
+                              className="object-cover"
                               width={84}
                               height={84}
                               alt={`${product?.name}`}
                             />
                             <Link
                               href={`product/${params.id}`}
-                              className='text-black'
+                              className="text-black"
                             >
                               <Title order={5}>{product?.name}</Title>
                             </Link>
                           </Flex>
                         </td>
-                        <td className='text-end'>
+                        <td className="text-end">
                           {product && numberFormat(product.price)}
                         </td>
-                        <td className='text-end'>
+                        <td className="text-end">
                           {numberFormat(Number(quantity))}
                         </td>
-                        <td className='text-end'>
+                        <td className="text-end">
                           {product && callTotalProce(product)}
                         </td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
-                <Divider variant='dashed' my='md' />
-                <Title order={3} className='text-end'>
+                <Divider variant="dashed" my="md" />
+                <Title order={3} className="text-end">
                   รวมเป็นเงินทั้งหมด
-                  <span className='text-primary mx-2'>
+                  <span className="text-primary mx-2">
                     {product && callTotalProce(product)}
                   </span>
                   บาท
                 </Title>
                 {product?.shop && (
                   <PaymentMethod
-                    className='mt-4'
+                    className="mt-4"
                     business_partner_id={product.shop.id}
                   />
                 )}
               </Card>
             )}
           </div>
-          <div className='w-full lg:w-80'>
-            {product && <OrderForm product={product} />}
+          <div className="w-full lg:w-80">
+            {/* {product && <OrderForm product={product} />} */}
           </div>
         </div>
       </div>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 import { cn } from "@/libs/utils";
 import { ActionIcon, Group, Navbar, ThemeIcon, Text } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
@@ -7,44 +9,109 @@ import {
   IconLayoutSidebarLeftCollapse,
   IconLayoutSidebarLeftExpand,
 } from "@tabler/icons-react";
-import { FoodIcon, MosqueIcon, ResortIcon, TravelIcon } from "./Icons";
+import {
+  FoodIcon,
+  MosqueIcon,
+  ResortIcon,
+  TravelIcon,
+  CalendarIcon,
+  MuseumIcon,
+  SouvenirIcon,
+  MarketIcon,
+  LinkOutIcon,
+} from "./Icons";
 
 type Props = {};
 
 const SideBar = ({}: Props) => {
   const [collapsed, { toggle: toggleCollapsed, close: closeCollapsed }] =
     useDisclosure(false);
-
   const APP_SHELL_MENUS = [
     {
-      key: "destination",
-      title: "สถานที่ท่องเที่ยว",
-      icon: <TravelIcon />,
+      key: "event",
+      title: "ปฏิทินกิจกรรม",
+      icon: <CalendarIcon />,
       color: "blue",
       active: "#EDF2FF",
+      slug: "/event",
     },
     {
       key: "hotel",
-      title: "ที่พักผ่อน",
+      title: "ที่พัก",
       icon: <ResortIcon />,
       color: "green",
       active: "#EBFBEE",
+      slug: "?t=hotel",
     },
     {
       key: "restaurant",
-      title: "อาหารจานโปรด",
+      title: "ร้านอาหารและคาเฟ่",
       icon: <FoodIcon />,
       color: "violet",
       active: "#F3F0FF",
+      slug: "?t=restaurant",
     },
     {
       key: "mosque",
       title: "มัสยิด",
       icon: <MosqueIcon />,
+      color: "#E6F6F1",
+      active: "#FFF4E6",
+      slug: "?t=mosque",
+    },
+    {
+      key: "museum",
+      title: "พิพิธภัณฑ์",
+      icon: <MuseumIcon />,
       color: "orange",
       active: "#FFF4E6",
+      slug: "",
+    },
+    {
+      key: "souvenir",
+      title: "ของฝาก",
+      icon: <SouvenirIcon />,
+      color: "#FFEBEB",
+      active: "#FFF4E6",
+      slug: "",
+    },
+    {
+      key: "market",
+      title: "แหล่งชอปปิ้ง / ตลาดนัด",
+      icon: <MarketIcon />,
+      color: "#E1E3F4",
+      active: "#FFF4E6",
+      slug: "",
+    },
+    {
+      key: "linkout1",
+      title: "สมัครเป็นผู้ประกอบการ",
+      icon: <LinkOutIcon />,
+      color: "#ffffff",
+      active: "#FFF4E6",
+      slug: "https://partners.admin.aowtani.com/login",
+      target: "_blank",
+    },
+    {
+      key: "linkout2",
+      title: "สมัครเป็นไกด์ท้องถิ่น",
+      icon: <LinkOutIcon />,
+      color: "#ffffff",
+      active: "#FFF4E6",
+      slug: "https://localguide.admin.aowtani.com/login",
+      target: "_blank",
     },
   ];
+
+  const activeMenu = (value: any) => {
+    return false;
+    // console.log({ searchParams, value });
+    // if (searchParams) {
+    //   return searchParams === value;
+    // } else {
+    //   return pathname === value;
+    // }
+  };
 
   return (
     <div
@@ -69,18 +136,20 @@ const SideBar = ({}: Props) => {
         {APP_SHELL_MENUS.map((menu) => (
           <Navbar.Section
             key={menu.title}
-            className={cn(`p-2 cursor-pointer`)}
-            //   bg={variant === menu.key ? menu.active : ""}
-            //   onClick={() =>
-            //     menu.key !== variant && router.push(`?t=${menu.key}`)
-            //   }
+            className={cn(`p-2 cursor-pointer text-black`)}
+            bg={activeMenu(menu.slug) ? menu.active : ""}
+            // onClick={() =>
+            //   menu.key !== variant && router.push(`?t=${menu.key}`)
+            // }
           >
-            <Group>
-              <ThemeIcon variant="light" color={menu.color}>
-                {menu.icon}
-              </ThemeIcon>
-              <Text>{menu.title}</Text>
-            </Group>
+            <Link href={menu.slug} target={menu.target ? menu.target : "_self"}>
+              <Group>
+                <ThemeIcon variant="light" color={menu.color}>
+                  {menu.icon}
+                </ThemeIcon>
+                <Text className="text-black">{menu.title}</Text>
+              </Group>
+            </Link>
           </Navbar.Section>
         ))}
       </Navbar>
